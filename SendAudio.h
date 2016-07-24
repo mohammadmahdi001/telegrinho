2cc55a8b2d4bd6920ba8e444e3cdee5f30c42d3d
mahdi <mohammadmahdi001@users.noreply.github.com>
2016-07-24 13:54:00 +0430

    Create SendAudio.h

---
+++ SendAudio.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <tgbot/tgbot.h>
+
+const std::string MISS_NORDESTINA_FILE_ID = "AwADAQADDAADK3zfBWVm9SUx9PswAg";
+
+const std::string MISS_NORDESTINA_CHAT[] = { "AwADAQADDAADK3zfBWVm9SUx9PswAg",
+											"AwADAQADSAADrH6LAQRB6681bnkTAg",
+											"AwADAQADqwEAAuoT2AABtwyyoAOwbxMC",
+											"AwADAQADSQADrH6LAYtRmiOHI6hmAg",
+											"AwADAQADVAEAAvs5HgUJgkaQPxBxXQI",
+											"AwADAQADSgADrH6LAejYeKwegYyhAg",
+											"AwADAQADrAEAAuoT2AABi5aIplglOIYC",
+											"AwADAQADVQEAAvs5HgWXCYzszMggjwI",
+											"AwADAQADrQEAAuoT2AABxylS5d28YKEC",
+											"AwADAQADSwADrH6LAfuixgv8CKjRAg",
+											"AwADAQADVgEAAvs5HgU3gXgPVnFqfAI",
+											"AwADAQADrgEAAuoT2AABRORQxRK6ReEC",
+											"AwADAQADTAADrH6LAZYNZ4DWSPJxAg",
+											"AwADAQADVwEAAvs5HgXJ6ca15HxXUAI",
+											"AwADAQADWAEAAvs5HgVHb8gFbLlyXAI",
+											"AwADAQADTQADrH6LAa_W1iv155HaAg",
+											"AwADAQADWQEAAvs5HgXpVLid-sAcrQI",
+											"AwADAQADWgEAAvs5HgVJ4m6e_e7iFAI",
+											"AwADAQADWwEAAvs5HgVLfGVNO0PiTgI",
+											"AwADAQADXAEAAvs5HgVBUIsKBJGq9AI" };
+
+const std::string CHUROS_FILE_ID = "AwADAQADDQADK3zfBTEk4HbTlM52Ag";
+
+const std::string RETCHA_GEMENDO_FILE_ID = "AwADAQADDgADK3zfBXTMW4j5cqevAg";
+const std::string XIS_CHUPANO_FILE_ID = "AwADAQADEAADK3zfBeb564h2rREyAg";
+const std::string TREVAH_FILE_ID = "AwADAQADDwADK3zfBSkdgqWBvnTgAg";
+const std::string MISS_NORDESTINA_DOLAR = "AwADAQADEQADK3zfBcJQnHZ8bUW-Ag";
+
+const std::string HASTAD_MEU_DEUS = "AwADAQADRAIAAuoT2AAB6GqK0bAYOqIC";
+
+void checkForAudio(TgBot::Message::Ptr message);
